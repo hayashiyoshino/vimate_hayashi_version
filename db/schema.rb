7267b81d7bbb51611b1149f43e65fe0eb618350c
林佳志乃 <twinklestaryoshino@gmail.com>
2019-09-21 16:01:12 +0900

    vimrcテーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_14_124048) do
+ActiveRecord::Schema.define(version: 2019_09_21_065601) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 2019_09_14_124048) do
     t.string "name", default: "", null: false
     t.text "uid", default: "", null: false
     t.string "oauth_token", default: "", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "vimrcs", force: :cascade do |t|
+    t.text "image_url", null: false
+    t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
