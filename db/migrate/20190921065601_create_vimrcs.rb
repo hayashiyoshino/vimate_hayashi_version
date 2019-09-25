0618b45c53977fa27f0572478c322007e7799bdc
林佳志乃 <twinklestaryoshino@gmail.com>
2019-09-25 12:50:28 +0900

    devise使ってusersテーブル作り直し

--- db/migrate/20190921065601_create_vimrcs.rb
+++ db/migrate/20190921065601_create_vimrcs.rb
@@ -1,8 +1,9 @@
 class CreateVimrcs < ActiveRecord::Migration[5.2]
   def change
     create_table :vimrcs do |t|
-      t.text :image_url, null: false
+      t.text :image_url,    null: false
       t.text :description
+      t.integer :user_id,   null: false
       t.timestamps
     end
   end
